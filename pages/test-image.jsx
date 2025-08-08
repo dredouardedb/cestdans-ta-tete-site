f06425dd15b25b7ae2146e71e76ed5207ae62b37
dredouardedb <dr.edouardedb@gmail.com>
2025-08-08 21:47:53 +0200

    Update test-image.jsx

--- pages/test-image.jsx
+++ pages/test-image.jsx
@@ -1,1 +1,19 @@
+export default function TestImage() {
+  return (
+    <main style={{padding: 24}}>
+      <h1 style={{fontFamily: 'sans-serif'}}>Test image externe</h1>
 
+      <p>Si tu vois une image juste en dessous, tout va bien côté affichage :</p>
+
+      <div style={{border: '2px solid red', padding: 12, marginTop: 12}}>
+        <img
+          src="https://picsum.photos/600/400"
+          alt="Test picsum"
+          style={{display: 'block', width: '100%', height: 'auto'}}
+        />
+      </div>
+
+      <p style={{marginTop: 12}}>Si cette image s'affiche, le souci vient de la page À propos (layout/CSS).</p>
+    </main>
+  );
+}
